40b05fcf46b730c50d420448fa1daf3062b8069e
Alex <almoav@gmail.com>
2015-07-12 10:07:58 -0700

    test loadfeed loads at least one entry

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -88,6 +88,7 @@ $(function() {
 	});
 
 	/* TODO: Write a new test suite named "Initial Entries" */
+	describe('Initial Entries', function() {
 
 		/* TODO: Write a test that ensures when the loadFeed
 		 * function is called and completes its work, there is at least
@@ -95,11 +96,22 @@ $(function() {
 		 * Remember, loadFeed() is asynchronous so this test wil require
 		 * the use of Jasmine's beforeEach and asynchronous done() function.
 		 */
+		beforeEach(function(done) {
+			loadFeed(0, function() {
+				done();
+			});
+		});
 
+		it('has at least one entry', function(done) {
+			expect(allFeeds.length).toBeGreaterThan(0);
+			done();
+		});
+		
 	/* TODO: Write a new test suite named "New Feed Selection"
 
 		/* TODO: Write a test that ensures when a new feed is loaded
 		 * by the loadFeed function that the content actually changes.
 		 * Remember, loadFeed() is asynchronous.
 		 */
+	});
 }());
